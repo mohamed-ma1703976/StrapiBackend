dc682df378734fcfb056e04bd0aec8a229e2d670
mohamedhossam23 <88920277+mohamed-ma1703976@users.noreply.github.com>
2022-12-26 18:05:30 +0300

    first

---
+++ src/api/surah/controllers/surah.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * surah controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::surah.surah');
---
+++ src/api/surah/routes/surah.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * surah router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::surah.surah');
